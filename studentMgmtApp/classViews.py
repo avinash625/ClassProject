0896058e533ccc156c5c7f6ea6578caaa700026a
avinash625 <avinashbondalapati@gmail.com>
2016-08-02 00:11:32 +0530

    added CreateClassView

--- studentMgmtApp/classViews.py
+++ studentMgmtApp/classViews.py
@@ -8,15 +8,21 @@ from studentMgmtApp.models import Teacher, Student, Class
 class TeacherCreateView(CreateView):
     model = Teacher
     fields = ["teacherName","teacherDept","classes"]
-    success_url = "/app/teacher/"
+    success_url = "/teacher/"
 
 class AddStudentView(CreateView):
     model = Student
     fields = ["studentName","studentYear","studentDept","studentClass"]
-    success_url = "/app/teacher/listStudents"
+    success_url = "/teacher/listStudents"
     widgets = {
         'StudentName': TextInput(),
     }
 
 class viewClasses(ListView):
     model = Class
+
+
+class CreateClass(CreateView):
+    model = Class
+    fields = ["className","classYear"]
+    success_url  = ""
--- studentMgmtApp/urls.py
+++ studentMgmtApp/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'teacher/addtest/$',view = TeacherAddTestView,name = "TeacherAddTestView"),
     url(r'teacher/addStudent/$',view = AddStudentView.as_view(),name = 'AddStudentView'),
     url(r'class/$',view = viewClasses.as_view(),name = "viewClasses"),
+    url(r'class/create/$',view = CreateClass.as_view(), name = "CreateClassView"),
     url(r'class/(?P<id>[0-9]+)/students/$',view = ClassStudentsView,name = "ClassStudentView"),
 ]
